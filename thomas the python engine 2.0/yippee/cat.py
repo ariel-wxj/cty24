144e20d2af13ad98c2d7d299480b4922efdac4b5
ariel-wxj <arielwang.wxj@gmail.com>
2024-06-24 20:09:07 -0400

    Add files via upload

---
+++ thomas the python engine 2.0/yippee/cat.py
@@ -0,0 +1,14 @@
+y = str(input("what is your name:"))
+
+x = int(input("what is your age:"))
+
+a = str(input("what month are you born in:"))
+
+str(x)
+
+print("hello", y ,", nice to meet you " )
+    
+print("you are", x ,"years old")
+
+print("born in ", a ," month")
+
